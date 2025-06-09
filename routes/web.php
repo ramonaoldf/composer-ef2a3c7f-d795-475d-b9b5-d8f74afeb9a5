1f54bd229ad4d635e15211364d22db79d152577a
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-08 22:42:12 -0300

    Vesion: 001.011.001.000

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\TrimStrings;
 use Illuminate\Support\Facades\Route;
 
-Route::post('webhook', 'WebhookController')->name('webhook');
+Route::post('webhook', 'WebhookController')
+    ->withoutMiddleware(TrimStrings::class)
+    ->name('webhook');
